a42a506d340164b33b6c0d8c4392607d0bbc8827
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2026-01-21 15:48:23 -0500

    Create scikit-learn.py

---
+++ scikit-learn.py
@@ -0,0 +1,16 @@
+#git clone 
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Load data
+df = pd.read_csv('light_data.csv')
+X = df[['Time']] # Feature
+y = df['LightLevel'] # Target
+
+# Train Model
+model = LinearRegression()
+model.fit(X, y)
+
+print(f"Model trained! Slope: {model.coef_[0]}", flush=True)
